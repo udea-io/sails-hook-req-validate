f5cbccef5edf114f05d120fa97c58cac3c2224b8
rsghost <im@rsghost.org>
2021-07-19 00:43:04 +0000

    test: 💍 base method converter

--- test/bdd/09.demo.test.js
+++ test/bdd/09.demo.test.js
@@ -1,21 +1,21 @@
-describe('09 Feature Test', function () {
+describe.only('09 Feature Test', function () {
   it('test', function (done) {
     sails.request({
       url: '/test',
       method: 'post',
     }, {
-      number: 1,
-      string: '1',
-      // string: 1,
-      nullable: '',
-      custom1: 1, // val > 0, sleep 1000ms
-      custom2: 2, // val > 1
-      // custom2: 1, // val > 1
-      custom3: 2, // val > 1, to string
+      boolean: 'true',
+      number: '1',
+      float: '2.3',
+      int: '4',
     }, function (err, res) {
       if (err) return done(err);
       res.statusCode.should.be.equal(200);
-      res.body.custom3.should.be.equal('2');
+      (typeof res.body.boolean).should.be.equal('boolean');
+      (typeof res.body.number).should.be.equal('number');
+      (typeof res.body.float).should.be.equal('number');
+      (typeof res.body.int).should.be.equal('number');
+
       return done();
     });
   });
