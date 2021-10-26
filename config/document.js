349dbc92d55ae28b0bd6eadddf6c0de30d04c25c
rsghost <im@rsghost.org>
2021-10-26 23:53:07 +0000

    refactor: 💡 document helper request/response struct

---
+++ config/document.js
@@ -0,0 +1,4 @@
+module.exports.document = {
+  requests: {},
+  responses: {},
+};
--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var validate = require('./lib/');
+const validate = require('./lib/');
+const document = require('./lib/document');
 
 module.exports = function(sails) {
   return {
     initialize: function(cb){
+      sails.document = document;
       sails.on('router:route', function(requestState) {
         requestState.req['validate'] = validate.bind(requestState);
       });
